0fafd431eb8320706a0f4bb9328a555f951ef414
Sebastian Dittrich <sebastian.dittrich@live.de>
2022-03-24 18:06:28 +0000

    Fixed top app bar

--- src/views/Task.vue
+++ src/views/Task.vue
@@ -1,34 +1,33 @@
 <template>
-    <div class="grid grid-rows-[1fr_auto] h-screen">
-        <div>
-            <div class="grid grid-cols-[auto_1fr_auto] gap-4 items-center p-4 bg-surface text-on-surface">
-                <i @click="$router.back()" class="hero outline arrow-left clickable-bg -m-4 p-4 text-lg"></i>
-                <div></div>
-                <TaskOptionsMenu :task="task">
-                    <template #default="{ open }">
-                        <i
-                            @click="open($event)"
-                            class="hero dots-vertical outline p-4 -m-4 clickable-bg rounded-full text-on-surface-variant"
-                        ></i>
-                    </template>
-                </TaskOptionsMenu>
-            </div>
-            <div class="grid grid-cols-[auto_1fr] gap-4 items-center mb-4 text-xl p-6 pt-4 bg-surface">
-                <DoneCheckbox :value="task.done" @input="task.update({ done: $event })" />
-                <input
-                    type="text"
-                    :value="task.title"
-                    @input="taskInput($event)"
-                    ref="input"
-                    class="bg-transparent w-full text-on-surface"
-                />
-            </div>
-            <div class="py-8">
-                <DeadlineIndicator :task="task" />
-            </div>
+    <div class="min-h-screen">
+        <div class="grid grid-cols-[auto_1fr_auto] gap-4 items-center p-4 bg-surface text-on-surface h-16">
+            <i
+                @click="$router.back()"
+                class="hero outline arrow-left clickable-bg -m-4 p-4 text-lg text-on-surface"
+            ></i>
+            <div></div>
+            <TaskOptionsMenu :task="task">
+                <template #default="{ open }">
+                    <i
+                        @click="open($event)"
+                        class="hero dots-vertical outline p-4 -m-4 clickable-bg rounded-full text-on-surface-variant"
+                    ></i>
+                </template>
+            </TaskOptionsMenu>
         </div>
-        <div>
-            <TaskActions :task="task" class="p-4" />
+        <div class="grid grid-cols-[auto_1fr] gap-4 items-center text-xl p-4 bg-surface">
+            <DoneCheckbox :value="task.done" @input="task.update({ done: $event })" />
+            <input
+                type="text"
+                :value="task.title"
+                @input="taskInput($event)"
+                ref="input"
+                class="bg-transparent w-full text-on-surface"
+            />
+        </div>
+        <TaskActions :task="task" class="p-4 pt-2" />
+        <div class="py-8">
+            <DeadlineIndicator :task="task" />
         </div>
     </div>
 </template>
